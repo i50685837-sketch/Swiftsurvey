bb44c9ca503673b8067aadf6315eabd403ab8144
i50685837-sketch <i50685837@gmail.com>
2026-07-24 19:02:32 +0300

    Create profile.js

---
+++ routes/profile.js
@@ -0,0 +1,20 @@
+// routes/profile.js
+
+const express = require("express");
+const router = express.Router();
+
+const auth = require("../middleware/auth");
+
+const {
+    getProfile
+} = require("../controllers/profileController");
+
+
+router.get(
+    "/",
+    auth,
+    getProfile
+);
+
+
+module.exports = router;
