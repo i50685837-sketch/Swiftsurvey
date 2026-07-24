0d0ac8eeb2060567158f8c5f0493ff9c70a3ff48
i50685837-sketch <i50685837@gmail.com>
2026-07-24 19:07:04 +0300

    Update server.js

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/profile", require("./routes/profile"));
+app.use("/api/withdrawals", require("./routes/withdrawals"));
 
 
 // Test API
